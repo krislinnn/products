0ac2ed6fec49ab78e96527510d66c2cdf9f65d4a
krislinnn <ohmygoood101@gmail.com>
2021-03-06 23:43:55 +0800

    add check file

--- products.py
+++ products.py
@@ -1,12 +1,19 @@
-#讀取檔案
+import os #operating system
+
+
 products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #繼續
-		name, price = line.strip().spilt(',') #切割完後直接存入name和price
-        products.append([name,price])
-print(products)
+if os.apth.isfile('product.csv'):
+	print('yeah! 找到檔案了!')
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #繼續
+			name, price = line.strip().spilt(',') #切割完後直接存入name和price
+			products.append([name,price])
+	print(products)
+else:
+	print('找不到檔案.....')
+
 		
 #讓使用者輸入
 while True:
